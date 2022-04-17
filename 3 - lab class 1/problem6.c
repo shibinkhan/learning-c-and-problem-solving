5a5c9ae82c3a140cf9f804027ade08385c622d5b
shibinkhan <khanshibin500@gmail.com>
2022-04-18 00:59:06 +0600

    updating

---
+++ 3 - lab class 1/problem6.c
@@ -0,0 +1,26 @@
+/*
+6. নিচের কোডটা কি করে বলে তোমার মনে হয়?
+
+#include <stdio.h>
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", x/y);
+    return 0;
+}
+
+কোডটাতে 5 0 ইনপুট দিয়ে দেখ তো কি হয়?
+*/
+
+#include <stdio.h>
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", x/y);
+
+    return 0;
+}
+
+/**
+০ দিয়ে কিছুকে ভাগ করা যায় না। চেস্টা করলে undefined বা can not divide by zero আসে। আর এখানে যে এরর টা দেখাচ্ছে এটাকে বলে "runtime error" এই এরর প্রোগ্রাম রান করার সময় ঘটে।
+**/
