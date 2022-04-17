a4aaca1d61c7e65b795080c7675747820864e3ba
shibinkhan <khanshibin500@gmail.com>
2022-04-17 21:45:57 +0600

    updating

---
+++ 2 - variable types and operators/circle.c
@@ -0,0 +1,28 @@
+/**
+
+**/
+
+#include<stdio.h>
+
+int main() {
+    /*
+    printf("Enter a radius: ");
+    double radius;
+    scanf("%lf", &radius);
+
+    const double PI = 3.1416;
+
+    double parimiter = 2 * PI * radius;
+    printf("Parimiter is %.2lf\n", parimiter);
+
+    double area = PI * (radius * radius);
+    printf("Area is %.2lf\n", area);
+    */
+
+    int a = 5, b = 2;
+	double q = (double)a/(double)b;
+	printf("%lf", q);
+
+	return 0;
+}
+
