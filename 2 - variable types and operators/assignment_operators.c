048eb3dd684cae0611ec0d8828d7d80ceb828c59
shibinkhan <khanshibin500@gmail.com>
2022-04-17 21:45:39 +0600

    updating

---
+++ 2 - variable types and operators/assignment_operators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int price = 20;
+
+    price = price + 10;
+    or price += 10;
+
+    price = price - 10;
+    or price -= 10;
+
+    price = price * 10;
+    or price *= 10;
+
+    price = price / 10;
+    or price /= 10;
+
+    return 0;
+}
