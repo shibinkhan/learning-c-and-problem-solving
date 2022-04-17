0195d70b3ec150a12f9467df4ad7688440d304b7
shibinkhan <khanshibin500@gmail.com>
2022-04-18 00:58:34 +0600

    updating

---
+++ 3 - lab class 1/problem2.c
@@ -0,0 +1,24 @@
+/*
+2. নিচের কোডের আউটপুট কি হবে বলে তোমার মনে হয়?
+
+#include <stdio.h>
+int main(){
+    printf ("Testing...");
+    printf ("....1");
+    printf ("...2");
+    printf ("..3");
+    printf ("\n");
+    return 0;
+}
+কোডটা রান করে দেখ তোমার উত্তর ঠিক আছে কিনা।
+*/
+
+#include <stdio.h>
+int main(){
+    printf ("Testing...");
+    printf ("....1");
+    printf ("...2");
+    printf ("..3");
+    printf ("\n");
+    return 0;
+}
