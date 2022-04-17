e579a167df07d1907000ab669ff65a0ffcce780b
shibinkhan <khanshibin500@gmail.com>
2022-04-18 00:59:26 +0600

    updating

---
+++ 3 - lab class 1/problem8.c
@@ -0,0 +1,9 @@
+/*
+8. একটা প্রোগ্রাম লিখ যেটা একটা আয়তক্ষেত্রের দৈর্ঘ্য আর প্রস্থ ইনপুট নিবে আর তারপর তার ক্ষেত্রফল আর পরিসীমা বের করে দিবে।
+Example:
+    Enter height of rectangle: 10
+    Enter width of rectangle: 30
+    Area is 300
+    Perimeter is 80
+*/
+
