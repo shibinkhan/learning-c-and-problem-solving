7649c389241c6a8f82adc415df7192f39c411139
shibinkhan <khanshibin500@gmail.com>
2022-04-17 21:45:03 +0600

    updating

---
+++ 2 - variable types and operators/variable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int price = 20;
+    printf("Price is %d\n", price);
+
+    float speed = 34.4;
+    printf("Speed is %f\n", speed);
+
+    double speed2 = 34.4;
+    printf("Speed2 is %f\n", speed2);
+
+    char character = 'q';
+    printf("Character is %c\n", character);
+
+    return 0;
+}
