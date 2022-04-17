38266f13d515c2bb7fc23afc89fbb61049cf1695
shibinkhan <khanshibin500@gmail.com>
2022-04-17 21:45:50 +0600

    updating

---
+++ 2 - variable types and operators/type_convertion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    // Implicit Type Conversion
+    double realNumber = 20.75;
+    int number = realNumber;
+    printf("number = %d\nrealNumber = %lf", number, realNumber);
+
+    // Explicit Type Convertion
+    // printf("number = %d\n", (int)realNumber);
+
+    return 0;
+}
+
