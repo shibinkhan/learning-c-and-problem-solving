41f1fed36506839ce8345a1c51893cb54ff5bc10
shibinkhan <khanshibin500@gmail.com>
2022-04-25 00:50:29 +0600

    updating

---
+++ 4 - control flow in c programming/simple_calculator.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Calculation: ");
+    scanf("%d %c %d", &num1, &op, &num2);
+
+    int result;
+    if(op == '+') {
+        result = num1 + num2;
+    }
+    else if(op == '-') {
+        result = num1 - num2;
+
+    }else if(op == '*') {
+        result = num1 * num2;
+
+    }else if(op == '/') {
+        result = num1 / num2;
+    }
+    printf("%d", result);
+
+    return 0;
+}
+
