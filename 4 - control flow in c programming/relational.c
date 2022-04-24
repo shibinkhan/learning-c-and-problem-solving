7ff15004bb13fa11841d8b71852809117f9c0b06
shibinkhan <khanshibin500@gmail.com>
2022-04-24 15:58:33 +0600

    updating

---
+++ 4 - control flow in c programming/relational.c
@@ -0,0 +1,53 @@
+// < <= >= > == !=
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+    int a = 28, b = 18;
+
+    /*
+    if(a < b) {
+        printf("a < b\n");
+    }
+
+    if(a <= b) {
+        printf("a <= b\n");
+    }
+
+    if(a > b) {
+        printf("a > b\n");
+    }
+
+    if(a >= b) {
+        printf("a >= b\n");
+    }
+
+    if(a == b) {
+        printf("a == b\n");
+    }
+
+    if(a != b) {
+        printf("a != b\n");
+    }
+
+    if(a*2 == b-2) {
+        printf("a == b\n");
+    }
+
+    int comp = a < b;
+    printf("%d\n", comp);
+
+    if(comp) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    */
+
+    bool condition = true;
+    printf("%d", condition);
+
+    return 0;
+}
