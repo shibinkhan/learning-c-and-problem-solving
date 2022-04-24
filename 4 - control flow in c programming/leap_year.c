b918f6cd1dc2bfa67283e762708f049a339e2a90
shibinkhan <khanshibin500@gmail.com>
2022-04-24 17:08:26 +0600

    updating

---
+++ 4 - control flow in c programming/leap_year.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+    printf("Year for Leap year test: ");
+    int year;
+    scanf("%d", &year);
+
+    bool is_leap_year = (year%4 == 0) && (year%100 != 0) || (year%400 == 0);
+    if(is_leap_year) {
+        printf("true\n");
+    }
+    else {
+        printf("false\n");
+    }
+
+    return 0;
+}
+
+/*
+শর্তঃ
+১। ৪ দিয়ে ভাগ দেওয়া যায়, ভাগশেষ থাকবে না
+২। ১০০ দিয়ে ভাগ দেওয়া যাবে না, ভাগশেষ থাকবে
+৩। ব্যাতিক্রমঃ ৪০০ এর গুনিতক হলে
+*/
