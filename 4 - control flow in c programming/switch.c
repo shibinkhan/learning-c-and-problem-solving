bbead42520949c7ce908927e86e4390960010f21
shibinkhan <khanshibin500@gmail.com>
2022-04-25 00:54:42 +0600

    updating

--- 4 - control flow in c programming/switch.c
+++ 4 - control flow in c programming/switch.c
@@ -3,6 +3,28 @@
 
 int main() {
     int num1, num2;
+    char op;
+
+    printf("Calculation: ");
+    scanf("%d %c %d", &num1, &op, &num2);
+
+    int result;
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+    printf("%d", result);
 
     return 0;
 }
+
