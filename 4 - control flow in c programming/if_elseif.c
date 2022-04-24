9121a671e86ec640dc5a332c847ecb40faee0448
shibinkhan <khanshibin500@gmail.com>
2022-04-24 16:20:21 +0600

    updating

--- 4 - control flow in c programming/if_elseif.c
+++ 4 - control flow in c programming/if_elseif.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if(a == 0){
+        printf("zero");
+    }
+    else if(a > 0) {
+        printf("positive");
+    }
+    else if(a == -10) {
+        printf("minus 10");
+    }
+    else {
+        printf("negative");
+    }
+
+    return 0;
+}
 
