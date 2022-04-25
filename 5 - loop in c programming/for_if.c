a676c7724e1bf58b5853c4b40ee17279d5f1e3ad
shibinkhan <khanshibin500@gmail.com>
2022-04-26 03:58:00 +0600

    updating

--- 5 - loop in c programming/for_if.c
+++ 5 - loop in c programming/for_if.c
@@ -1,28 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    /*
-    for(int i=1; i<=10; i++){
-        if(i%2 == 0) {
-            printf("%d is even\n", i);
-        }
-        else{
-            printf("%d is odd\n", i);
-        }
-    }
-    */
-
-    int n;
-    printf("Number: ");
-    scanf("%d", &n);
-
-    int count;
-    for(int i=1; i<=n; i++){
-        if(n%i == 0) {
-            printf("%d\n", i);
-            count++;
-        }
-    }
-    printf("How much Divisors = %d\n", count);
-    return 0;
+	for (int i=0; i<3; i++) {
+    	for (int j=0; j<5; j++) {
+        	printf("*");
+    	}
+	}
 }
+
