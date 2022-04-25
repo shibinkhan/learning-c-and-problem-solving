917fffd3e2e386a6121e6b63b54a6da9073f718c
shibinkhan <khanshibin500@gmail.com>
2022-04-25 15:10:49 +0600

    updating

---
+++ 5 - loop in c programming/usage_of_for_loop.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+    printf("Number: ");
+    int n;
+    scanf("%d", &n);
+
+
+
+    /*
+    // multi
+    int sum = 1;
+
+    for(int i=1; i<=n; i++) {
+        sum *= i;
+        printf("i = %d sum = %d\n", i, sum);
+    }
+    printf("Final Sum = %d", sum);
+    */
+
+    /*
+    // sum
+    int sum = 0;
+
+    for(int i=1; i<=n; i++) {
+        sum += i;
+        printf("i = %d sum = %d\n", i, sum);
+    }
+    printf("Final Sum = %d", sum);
+    */
+
+    return 0;
+}
