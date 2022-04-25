bb209e4dc7c55ec9d30b8671c21ad25afc0a61d5
shibinkhan <khanshibin500@gmail.com>
2022-04-25 14:30:20 +0600

    updating

---
+++ 5 - loop in c programming/for_loop_variants.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int i=1, j=9;
+    // for(; i<=9, j>=1; i++, j--) {
+    for(;;) {
+        printf("Hello World\n");
+    }
+    // printf("Last: %d %d\n", i, j);
+
+    return 0;
+}
