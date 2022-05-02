958ff04dd1e74d41d54c43516db3ca365ae59658
shibinkhan <khanshibin500@gmail.com>
2022-05-02 07:02:36 +0600

    updating

--- 6 - lab class 2/problem_c.c
+++ 6 - lab class 2/problem_c.c
@@ -11,6 +11,30 @@ int main() {
         int used;
         scanf("%d", &used);
 
+        int month_limit = total_limit + rest;
+        rest = month_limit - used;
+
+        // printf("Limit= %d, Used= %d, Rest= %d\n", month_limit, used, rest);
+    }
+    int final_limit = total_limit + rest;
+    printf("%d", final_limit);
+
+    return 0;
+}
+
+/*
+//way 2
+#include<stdio.h>
+
+int main() {
+    int total_limit, months;
+    scanf("%d %d", &total_limit, &months);
+
+    int rest = 0;
+    for(int i=1; i<=months; i++) {
+        int used;
+        scanf("%d", &used);
+
         rest += (total_limit - used);
     }
     int total_rest = total_limit + rest;
@@ -18,3 +42,4 @@ int main() {
 
     return 0;
 }
+*/
