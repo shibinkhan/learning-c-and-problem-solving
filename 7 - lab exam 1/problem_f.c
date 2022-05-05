66513b363cdf12e532cd6f19c62a57aa6acfee27
shibinkhan <khanshibin500@gmail.com>
2022-05-06 03:52:14 +0600

    updating

--- 7 - lab exam 1/problem_f.c
+++ 7 - lab exam 1/problem_f.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
 int main() {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
 
+    if (A+B+C == 17) {
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
 
     return 0;
 }
