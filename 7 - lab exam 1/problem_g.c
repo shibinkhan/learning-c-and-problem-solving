473352c10f56cad0e7be1c03f5fedd20e1466f00
shibinkhan <khanshibin500@gmail.com>
2022-05-06 04:07:19 +0600

    updating

--- 7 - lab exam 1/problem_g.c
+++ 7 - lab exam 1/problem_g.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
 int main() {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
 
+    if (C>=A && C<=B) {
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
 
     return 0;
 }
