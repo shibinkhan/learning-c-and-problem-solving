8da57d3310469f4ac6da604ee742132e08313c06
shibinkhan <khanshibin500@gmail.com>
2022-05-06 05:06:54 +0600

    updating

--- 7 - lab exam 1/problem_i.c
+++ 7 - lab exam 1/problem_i.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 
 int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
 
+    int biggest;
+    if(a >= b && a >= c) {
+        biggest = a;
+    }
+    if(b >= a && b >= c) {
+        biggest = b;
+    }
+    if(c >= a && c >= b) {
+        biggest = c;
+    }
+
+    int output = (a+b+c) - biggest;
+    printf("%d", output);
 
     return 0;
 }
