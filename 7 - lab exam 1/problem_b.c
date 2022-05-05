bf23fba02a9e2a17b44717df5a231ed2a293e684
shibinkhan <khanshibin500@gmail.com>
2022-05-06 02:35:36 +0600

    updating

--- 7 - lab exam 1/problem_b.c
+++ 7 - lab exam 1/problem_b.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
 int main() {
+    int number;
+    scanf("%d", &number);
 
+    for(int i=1; i<=number; i++) {
+        printf("%d Abracadabra\n", i);
+    }
 
     return 0;
 }
