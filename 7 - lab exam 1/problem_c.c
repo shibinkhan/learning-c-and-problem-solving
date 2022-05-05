d1be2291ef7a559d78addbe483defcb02f445de1
shibinkhan <khanshibin500@gmail.com>
2022-05-06 03:04:12 +0600

    updating

--- 7 - lab exam 1/problem_c.c
+++ 7 - lab exam 1/problem_c.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 
 int main() {
+    int numbOfTemp;
+    scanf("%d", &numbOfTemp);
 
+    int numbOfMinusTemp = 0;
+    for(int i=1; i<=numbOfTemp; i++) {
+        int temp;
+        scanf("%d", &temp);
+
+        if(temp < 0) {
+            numbOfMinusTemp++;
+        }
+    }
+    printf("%d", numbOfMinusTemp);
 
     return 0;
 }
--- 7 - lab exam 1/problem_d.c
+++ 7 - lab exam 1/problem_d.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 
 int main() {
+    int numbOfCalc;
+    scanf("%d", &numbOfCalc);
 
+    double totalSum = 0;
+    for(int i=1; i<=numbOfCalc; i++) {
+        double num1, num2;
+        scanf("%lf %lf", &num1, &num2);
+
+        double multi = num1 * num2;
+        // printf("%lf\n", multi);
+        totalSum += multi;
+    }
+    printf("%.3lf", totalSum);
 
     return 0;
 }
