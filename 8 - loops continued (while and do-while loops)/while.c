a4c94fb43da3f07d2523beb924e66ca5cb7ea47d
shibinkhan <khanshibin500@gmail.com>
2022-05-08 07:46:19 +0600

    updating

--- 8 - loops continued (while and do-while loops)/while.c
+++ 8 - loops continued (while and do-while loops)/while.c
@@ -1,14 +1,27 @@
 #include<stdio.h>
 
 int main() {
-    for(int i = 1; i<=5; i++) {
+    /*
+    // for
+    for (int i = 1; i<=5; i++) {
         printf("%d\n", i);
     }
 
+    // while
     int i = 1;
-    while(i<=5) {
+    while (i<=5) {
         printf("%d\n", i);
         i++;
+    }
+    */
+    int money = 0;
+    int target = 100;
+    while (money < target) {
+        printf("Give more money: ");
+        int given;
+        scanf("%d", &given);
+        money += given;
+        printf("Current money = %d\n", money);
     }
 
     return 0;
