170a2b6311a166280358e885949c9eb315d449c5
shibinkhan <khanshibin500@gmail.com>
2022-05-11 23:00:52 +0600

    updating

--- 8 - loops continued (while and do-while loops)/gcd.c
+++ 8 - loops continued (while and do-while loops)/gcd.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
 int main() {
-    // printf("Numbers: ");
+    printf("Numbers: ");
     int a, b;
-    // scanf("%d %d", &a, &b);
+    scanf("%d %d", &a, &b);
 
     while(a != 0) {
         int remain = b%a;
         a = remain;
         b = a;
-        // printf("%d %d\n", a, b);
+        printf("%d %d\n", a, b);
     }
-    // printf("GCD = %d\n", b);
+    printf("GCD = %d\n", b);
 
     return 0;
 }
