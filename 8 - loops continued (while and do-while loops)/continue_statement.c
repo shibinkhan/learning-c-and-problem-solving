91964bc78cd86d7b76b66de0eaced6aa1371e289
shibinkhan <khanshibin500@gmail.com>
2022-05-14 13:30:10 +0600

    updating

--- 8 - loops continued (while and do-while loops)/continue_statement.c
+++ 8 - loops continued (while and do-while loops)/continue_statement.c
@@ -1,13 +1,15 @@
+/*
+// for
+
 #include<stdio.h>
-#include<stdbool.h>
 
 int main() {
-    for (int i = 1; i<=10; i++) {
-        if (i%3 == 0) {
-            continue;
-        }
+    for(int i = 1; i<=10; i++) {
+        if (i%3 == 0) continue;
+
         printf("%d = ", i);
-        for (int j = 1; j<=10; j++) {
+
+        for(int j = 1; j<=10; j++) {
             printf("%d ", i*j);
         }
         printf("\n");
@@ -16,3 +18,24 @@ int main() {
     return 0;
 }
 
+*/
+
+// while
+
+#include<stdio.h>
+
+int main() {
+    int num = 1234;
+
+    int sum = 0;
+    while(num > 0) {
+        int lastDigit = num%10;
+        num /= 10;
+
+        if(lastDigit%2==0) continue;
+        sum += lastDigit;
+    }
+    printf("Sum = %d", sum);
+
+    return 0;
+}
