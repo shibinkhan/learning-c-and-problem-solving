f025ac80a196a009a383cf06ef2abf35f3a7c14b
shibinkhan <khanshibin500@gmail.com>
2022-05-14 14:54:41 +0600

    updating

--- 8 - loops continued (while and do-while loops)/do_while_loop.c
+++ 8 - loops continued (while and do-while loops)/do_while_loop.c
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 
 int main() {
@@ -10,4 +11,18 @@ int main() {
     printf("Your input: %d", num);
 
     return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main() {
+  int i = 6;
+
+  do {
+    printf("%d\n", i);
+    i++;
+  } while (i < 5);
+
+  return 0;
 }
