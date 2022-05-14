8ec93b1a78dfa4bc0416ab7dedb1001b9585b167
shibinkhan <khanshibin500@gmail.com>
2022-05-14 12:19:12 +0600

    updating

--- 8 - loops continued (while and do-while loops)/break_statement.c
+++ 8 - loops continued (while and do-while loops)/break_statement.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
 
+int main() {
+    while(true) {
+        int input;
+        scanf("%d", &input);
+
+        if(input != 0 && input != 1) {
+            break;
+        }
+        printf("Input: %d\n", input);
+    }
+
+    return 0;
+}
