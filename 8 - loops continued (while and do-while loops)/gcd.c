9ea9a665016b9615102a0d292049d6b8e966f176
shibinkhan <khanshibin500@gmail.com>
2022-05-14 10:55:38 +0600

    updating

--- 8 - loops continued (while and do-while loops)/gcd.c
+++ 8 - loops continued (while and do-while loops)/gcd.c
@@ -5,6 +5,20 @@ int main() {
     int a, b;
     scanf("%d %d", &a, &b);
 
+    int gcd = 1;
+    for(int i=1; i<=a && i<=b; i++) {
+        if((a%i == 0) && (b%i == 0)) {
+            printf("Common Factors: %d\n", i);
+            gcd = i;
+        }
+    }
+    printf("GCD: %d\n", gcd);
+
+/*
+    printf("Numbers: ");
+    int a, b;
+    scanf("%d %d", &a, &b);
+
     while(a != 0) {
         int remain = b%a;
         a = remain;
@@ -12,6 +26,6 @@ int main() {
         printf("%d %d\n", a, b);
     }
     printf("GCD = %d\n", b);
-
+*/
     return 0;
 }
