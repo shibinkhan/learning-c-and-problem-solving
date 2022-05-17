548e4e4a6517bc56c30648e74f50e82420e4b4c8
shibinkhan <khanshibin500@gmail.com>
2022-05-17 14:49:03 +0600

    updating

--- 9 - revisit (variables, operators, data-types, conditional statements and loops)/float.c
+++ 9 - revisit (variables, operators, data-types, conditional statements and loops)/float.c
@@ -8,12 +8,17 @@ int main() {
     double num2 = 20.41;
     // printf("%lf\n", num2);
 
-    double num3 = 100.0;
+    int num3 = 100;
+
     double square = pow(num3, 2);
     printf("%lf\n", square);
 
     double cube = pow(num3, 3);
     printf("%lf\n", cube);
+
+    int root = sqrt(num3);
+    printf("%d\n", root);
+
 
     return 0;
 }
