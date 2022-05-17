e5fd7314c04e84214037f0d22493b932eb942a8f
shibinkhan <khanshibin500@gmail.com>
2022-05-17 13:00:51 +0600

    updating

--- 8 - loops continued (while and do-while loops)/guessing_game.c
+++ 8 - loops continued (while and do-while loops)/guessing_game.c
@@ -4,11 +4,30 @@
 int main() {
     srand(time(0));
     int hidden = rand()%100+1;
-    printf("%d", hidden);
+    // printf("%d\n", hidden);
+    printf("Guess the number...\n");
 
-    while(1) {
+    int numbOfGuess = 0;
+    while (numbOfGuess < 10) {
+        int guess;
+        scanf("%d", &guess);
 
+        if (hidden == guess) {
+            printf("You are right\n");
+            break;
+        }
+        else if (hidden > guess) {
+            printf("Guess larger\n");
+        }
+        else if (hidden < guess) {
+            printf("Guess smaller\n");
+        }
+
+        numbOfGuess ++;
     }
+    if(numbOfGuess == 10) {
+            printf("You Failed\n");
+        }
 
     return 0;
 }
