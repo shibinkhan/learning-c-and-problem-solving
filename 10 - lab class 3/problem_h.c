af6b29f47677e6e1adb8a8ed1a4a403ba186637d
shibinkhan <khanshibin500@gmail.com>
2022-05-18 00:29:52 +0600

    updating

--- 10 - lab class 3/problem_h.c
+++ 10 - lab class 3/problem_h.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int years = 0;
+
+    while (a<=b) {
+        a *= 3;
+        b *= 2;
+        years ++;
+    }
+    printf("%d", years);
+
+    return 0;
+}
