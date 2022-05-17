2470e8f6b1a80fdf5ebb162ce50e5307a34735ac
shibinkhan <khanshibin500@gmail.com>
2022-05-17 16:49:41 +0600

    updating

--- 9 - revisit (variables, operators, data-types, conditional statements and loops)/loop.c
+++ 9 - revisit (variables, operators, data-types, conditional statements and loops)/loop.c
@@ -3,8 +3,9 @@
 int main() {
     for (int i=1; i<=10; i++) {
             for (int j=1; j<=10; j++) {
-                printf("%d", i*j);
+                printf("%d\t", i*j);
         }
+        printf("\n");
     }
 
     return 0;
