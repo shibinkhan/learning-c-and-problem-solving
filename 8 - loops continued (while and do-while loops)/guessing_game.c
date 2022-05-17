611d00c5a2ca50fb5786586977517d315219cef5
shibinkhan <khanshibin500@gmail.com>
2022-05-17 12:42:27 +0600

    updating

--- 8 - loops continued (while and do-while loops)/guessing_game.c
+++ 8 - loops continued (while and do-while loops)/guessing_game.c
@@ -2,8 +2,13 @@
 #include<stdlib.h>
 
 int main() {
-    //srand(time(0));
-    int hidden = rand();
+    srand(time(0));
+    int hidden = rand()%100+1;
     printf("%d", hidden);
+
+    while(1) {
+
+    }
+
     return 0;
 }
