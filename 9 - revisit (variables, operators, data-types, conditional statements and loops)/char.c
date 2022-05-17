7e702787bc10cda7af397b26f87f2ce1c0666682
shibinkhan <khanshibin500@gmail.com>
2022-05-17 15:49:31 +0600

    updating

--- 9 - revisit (variables, operators, data-types, conditional statements and loops)/char.c
+++ 9 - revisit (variables, operators, data-types, conditional statements and loops)/char.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
 int main() {
-    int num = 65;
-    printf("%c ", num);
-    printf("%d", num);
+    char ch;
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <='Z') {
+        printf("Uppercase");
+    }
+    if (ch >= 'a' && ch <='z') {
+        printf("Lowercase");
+    }
+    if (ch >= '0' && ch <='9') {
+        printf("Digit");
+    }
 
     return 0;
 }
