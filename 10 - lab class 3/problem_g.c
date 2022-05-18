8fd916d6dc2725cb42115eeea7087a712be9b3bf
shibinkhan <khanshibin500@gmail.com>
2022-05-18 12:30:13 +0600

    updating

--- 10 - lab class 3/problem_g.c
+++ 10 - lab class 3/problem_g.c
@@ -4,10 +4,8 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a == b) {
-        if (b == c) {
+    if (a == b && b == c) {
             printf("No");
-        }
     }
     else if (a != b && b != c && a != c) {
         printf("No");
