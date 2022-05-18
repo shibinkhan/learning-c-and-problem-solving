ca155903893d754f155bee56cfea2864d388efec
shibinkhan <khanshibin500@gmail.com>
2022-05-18 12:54:46 +0600

    updating

---
+++ 10 - lab class 3/restoring_3_nums.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c) {
+            printf("No");
+    }
+    else if (a != b && b != c && a != c) {
+        printf("No");
+    }
+    else {
+        printf("Yes");
+    }
+
+    return 0;
+}
+
