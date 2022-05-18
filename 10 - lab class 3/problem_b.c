3276db9ac7b35890713795d7114c940699b5d404
shibinkhan <khanshibin500@gmail.com>
2022-05-18 12:07:23 +0600

    updating

--- 10 - lab class 3/problem_b.c
+++ 10 - lab class 3/problem_b.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
 int main() {
+    int x = 0;
 
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++) {
+        char a, b, c;
+        scanf(" %c%c%c", &a, &b, &c);
+        // printf("p: %c%c%c\n", a, b, c);
+
+        if (b=='+') x++;
+        else if (b=='-') x--;
+    }
+    printf("%d", x);
 
     return 0;
 }
