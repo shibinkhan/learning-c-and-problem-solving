9d77c6aef6dd53bdc1a5524aa9db9a778ec2c60f
shibinkhan <khanshibin500@gmail.com>
2022-05-18 12:56:17 +0600

    updating

--- 10 - lab class 3/restoring_3_nums.c
+++ 10 - lab class 3/restoring_3_nums.c
@@ -1,18 +1,19 @@
+// 10-4 Solving Restoring Three Numbers
+// https://codeforces.com/problemset/problem/1154/A
+
 #include<stdio.h>
 
 int main() {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
 
-    if (a == b && b == c) {
-            printf("No");
-    }
-    else if (a != b && b != c && a != c) {
-        printf("No");
-    }
-    else {
-        printf("Yes");
-    }
+    int sum = 0;
+    if (a > sum) sum = a;
+    if (b > sum) sum = b;
+    if (c > sum) sum = c;
+    if (d > sum) sum = d;
+
+    printf("%d", sum);
 
     return 0;
 }
