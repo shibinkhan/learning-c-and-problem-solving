1ce7efd7f842799de107d2a1392e52eb9a5f0f84
shibinkhan <khanshibin500@gmail.com>
2022-05-20 00:18:03 +0600

    updating

--- 11 - exam day/problem_g.c
+++ 11 - exam day/problem_g.c
@@ -1,7 +1,41 @@
+
+// way 1
 #include<stdio.h>
 
 int main() {
-    int
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int ans;
+    if (a > b) {
+        ans = 0;
+    }
+    else {
+        ans = (b - a) + 1;
+    }
+
+    printf("%d", ans);
 
     return 0;
 }
+
+/*
+// way 2
+#include<stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int ans = (b - a) + 1;
+    int ans2;
+
+    while (ans2 < ans) {
+        ans2 ++;
+    }
+
+    printf("%d", ans2);
+
+    return 0;
+}
+/*
