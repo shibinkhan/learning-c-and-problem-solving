e4d65d2b8962a1f7fe909c91ea6417c450c53b1c
shibinkhan <khanshibin500@gmail.com>
2022-05-22 13:04:53 +0600

    updating

--- 11 - exam day/problem_i.c
+++ 11 - exam day/problem_i.c
@@ -4,6 +4,21 @@ int main() {
     int x;
     scanf("%d", &x);
 
+    if (x>=90) {
+        printf("expert");
+    }
+    if (x>=70 && x<90) {
+        int ans = 90 - x;
+        printf("%d", ans);
+    }
+    if (x>=40 && x<70) {
+        int ans = 70 - x;
+        printf("%d", ans);
+    }
+    if (x>=0 && x<40) {
+        int ans = 40 - x;
+        printf("%d", ans);
+    }
 
     return 0;
 }
