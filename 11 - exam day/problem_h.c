0b53278e0a72a2b4895318cadcde42e74d967389
shibinkhan <khanshibin500@gmail.com>
2022-05-22 12:53:05 +0600

    updating

--- 11 - exam day/problem_h.c
+++ 11 - exam day/problem_h.c
@@ -6,26 +6,7 @@ int main() {
 
     for (int i=1; i<=n; i++) {
 
-        if (i%2==1) {
-            if (i==n) {
-                printf("I hate ");
-            }
-            else {
-                printf("I hate that ");
-            }
-        }
 
-        if (i%2==0) {
-            if (i==n) {
-                printf("I love ");
-            }
-            else {
-                printf("I love that ");
-            }
-        }
-
-
-    }
     printf("it");
 
     return 0;
