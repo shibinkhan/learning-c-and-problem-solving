da5c4725d7b97d6b1bd8d0f791f1b75a599838cb
shibinkhan <khanshibin500@gmail.com>
2022-05-22 12:52:46 +0600

    updating

--- 11 - exam day/problem_h.c
+++ 11 - exam day/problem_h.c
@@ -1,7 +1,32 @@
 #include<stdio.h>
 
 int main() {
+    int n;
+    scanf("%d", &n);
 
+    for (int i=1; i<=n; i++) {
+
+        if (i%2==1) {
+            if (i==n) {
+                printf("I hate ");
+            }
+            else {
+                printf("I hate that ");
+            }
+        }
+
+        if (i%2==0) {
+            if (i==n) {
+                printf("I love ");
+            }
+            else {
+                printf("I love that ");
+            }
+        }
+
+
+    }
+    printf("it");
 
     return 0;
 }
