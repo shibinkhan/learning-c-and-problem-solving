e6b1fe022e73ccc9447cc70de2f50d27909c05b2
shibinkhan <khanshibin500@gmail.com>
2022-05-26 01:06:59 +0600

    updating

--- 11 - exam day/problem_e.c
+++ 11 - exam day/problem_e.c
@@ -36,16 +36,6 @@ int main() {
         printf("Draw");
     }
 
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
 */
@@ -55,6 +45,58 @@ int main() {
 int main() {
     int a, b, c, d, e, f, x;
     scanf("%d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &x);
+
+    int x2 = x;
+    int run1 = 0;
+    while (x != 0) {
+        if (x>=a) {
+            run1 += a;
+            x -= a;
+        }
+        else {
+            run1 += x;
+            x = 0;
+        }
+        //rest1
+        if (x>=c) {
+            x -= c;
+        }
+        else {
+            x = 0;
+        }
+    }
+
+    int run2 = 0;
+    while (x2 != 0) {
+        if (x2>=d) {
+            run2 += d;
+            x2 -= d;
+        }
+        else {
+            run2 += x2;
+            x2 = 0;
+        }
+        //rest2
+        if (x2>=f) {
+            x2 -= f;
+        }
+        else {
+            x2 = 0;
+        }
+    }
+
+    int total1 = run1 * b;
+    int total2 = run2 * e;
+
+    if (total1 > total2) {
+        printf("Takahashi");
+    }
+    else if (total1 < total2) {
+        printf("Aoki");
+    }
+    else {
+        printf("Draw");
+    }
 
     return 0;
 }
