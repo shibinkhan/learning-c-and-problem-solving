e1ed55a1663084e1ebd25fbfbd3fe2eac62082d0
shibinkhan <khanshibin500@gmail.com>
2022-05-26 01:43:53 +0600

    updating

--- 11 - exam day/problem_c.c
+++ 11 - exam day/problem_c.c
@@ -50,5 +50,18 @@ int main() {
     int s, t, x;
     scanf("%d %d %d", &s, &t, &x);
 
+    if(s<=x && t>x) {
+        printf("Yes");
+    }
+    else if(s>=x && t>x) {
+        printf("Yes");
+    }
+    else if(s<=x && t<x && s>t) {
+        printf("Yes");
+    }
+    else {
+         printf("No");
+    }
+
     return 0;
 }
