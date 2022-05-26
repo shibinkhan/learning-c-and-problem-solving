b247ae4421ac4d50c91cccfdb9f1b99bc0f9c2d1
shibinkhan <khanshibin500@gmail.com>
2022-05-26 17:06:23 +0600

    updating

--- 12 - array/array.c
+++ 12 - array/array.c
@@ -6,6 +6,7 @@ int main() {
     for (int i=0; i<=2; i++) {
         printf("Enter student %d Mark: ", i+1);
         scanf("%d", &mark[i]);
+
         mark[i] += 5;
         printf("Student %d got %d\n", i+1, mark[i]);
     }
