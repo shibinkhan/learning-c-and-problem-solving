156a3a36014f98ca7dd2d42dc1dc7f59b2917753
shibinkhan <khanshibin500@gmail.com>
2022-05-26 18:56:55 +0600

    updating

--- 12 - array/init.c
+++ 12 - array/init.c
@@ -1,7 +1,36 @@
+/*
 #include<stdio.h>
 
 int main() {
+    const int n;
+    scanf("%d", &n);
+    int values[n];
 
+    for (int i=0; i<n; i++) {
+        values[i] = i+1;
+
+    }
+    values[4] *= 5;
+    values[6] = values[2] + values[5];
+
+    for (int i=0; i<n; i++) {
+        printf("values[%d] = %d\n", i, values[i]);
+    }
+
+
+    return 0;
+}
+*/
+
+#include<stdio.h>
+
+int main() {
+    const int n;
+    scanf("%d", &n);
+    int values[n];
+
+    int arraySize = sizeof(values) / sizeof(0);
+    printf("%d\n", arraySize);
 
     return 0;
 }
