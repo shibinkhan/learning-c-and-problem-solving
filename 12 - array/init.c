5ac5de17d25d436f9cf87c38827972f0d3dd4608
shibinkhan <khanshibin500@gmail.com>
2022-05-31 14:34:27 +0600

    updating

--- 12 - array/init.c
+++ 12 - array/init.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 
 int main() {
-    const int n;
-    scanf("%d", &n);
-    int values[n];
+    char word[] = {'s', 'h', 'i', 'b', 'i', 'n'};
+
+    int sz = sizeof(word) / sizeof(word[0]);
+    for (int i=0; i<sz; i++) {
+        printf("%c", word(i));
+    }
 
     return 0;
 }
