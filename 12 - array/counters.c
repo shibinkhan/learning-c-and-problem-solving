cf5163898bcf62924b2bdac8d54053b1498daa6c
shibinkhan <khanshibin500@gmail.com>
2022-05-31 16:42:58 +0600

    updating

--- 12 - array/counters.c
+++ 12 - array/counters.c
@@ -2,17 +2,21 @@
 
 int main() {
     int numOfUsers;
+    printf("Num of Users: ");
     scanf("%d", &numOfUsers);
 
     int ratings[numOfUsers];
     for (int i=0; i<numOfUsers; i++) {
-        int rating;
-        scanf("%d", &rating);
-        ratings[i] = rating;
+        scanf("%d", &ratings[i]);
     }
+
+    int sum = 0;
     for (int i=0; i<numOfUsers; i++) {
-        printf("%d", ratings[i]);
+        // printf("%d ", ratings[i]);
+        sum += ratings[i];
     }
+    double avarage = (double)sum / numOfUsers;
+    printf("%.2lf ", avarage);
 
     return 0;
 }
