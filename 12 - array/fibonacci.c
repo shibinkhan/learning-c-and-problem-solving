861836137153920f3c8a15554be15d60b366dc7a
shibinkhan <khanshibin500@gmail.com>
2022-05-31 15:17:53 +0600

    updating

--- 12 - array/fibonacci.c
+++ 12 - array/fibonacci.c
@@ -15,8 +15,8 @@ int main() {
     for (int i=2; i<n; i++) {
         nums[i] = nums[i-2] + nums[i-1];
     }
-    for (int i=2; i<n; i++) {
-        printf("%d ", nums[i]);
+    for (int i=0; i<n; i++) {
+        printf("Num %d = %d\n", i+1, nums[i]);
     }
 
     return 0;
