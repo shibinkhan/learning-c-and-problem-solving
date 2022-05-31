60484018a1ad9e1297412fb64bc5d0ca88da212d
shibinkhan <khanshibin500@gmail.com>
2022-05-31 15:02:43 +0600

    updating

--- 12 - array/var_length_array.c
+++ 12 - array/var_length_array.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
 int main() {
+    int students;
+    printf("Enter num of Students: ");
+    scanf("%d", &students);
+    int mark[3];
 
+    for (int i=0; i<students; i++) {
+        printf("Enter student %d Mark: ", i+1);
+        scanf("%d", &mark[i]);
+
+        mark[i] += 5;
+        printf("Student %d got %d\n", i+1, mark[i]);
+    }
 
     return 0;
 }
