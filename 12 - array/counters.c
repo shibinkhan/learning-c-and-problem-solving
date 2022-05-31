3a07ebd6e7c6e93154256e3dfed4f878d2316101
shibinkhan <khanshibin500@gmail.com>
2022-05-31 16:37:37 +0600

    updating

--- 12 - array/counters.c
+++ 12 - array/counters.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
 int main() {
+    int numOfUsers;
+    scanf("%d", &numOfUsers);
 
+    int ratings[numOfUsers];
+    for (int i=0; i<numOfUsers; i++) {
+        int rating;
+        scanf("%d", &rating);
+        ratings[i] = rating;
+    }
+    for (int i=0; i<numOfUsers; i++) {
+        printf("%d", ratings[i]);
+    }
 
     return 0;
 }
