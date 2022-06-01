022488b5d8aefb3f3bb0d077eb08923ae07bb101
shibinkhan <khanshibin500@gmail.com>
2022-06-01 23:01:11 +0600

    updating

--- 13 - string/strcpy.c
+++ 13 - string/strcpy.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
-    char str[] = "Hello";
+    char str[] = "Hello World";
     char str2[10];
 
+    /*
     int length = strlen(str);
     for (int i=0; i<length; i++) {
         str2[i] = str[i];
     }
     str2[length] = '\0';
+    */
+
+    int length = strlen(str);
+    strncpy(str2, str, 9);
+    str2[9] = '\0';
 
     printf("%s", str2);
 
