a395a2eab38b9c39122ec96bb2f32be46ddd87fe
shibinkhan <khanshibin500@gmail.com>
2022-06-01 15:24:57 +0600

    updating

--- 13 - string/string.c
+++ 13 - string/string.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 int main() {
+    char st[] = "This is a string.";
 
+    int sz = sizeof(st) / sizeof(st[0]);
+    printf("%d\n", sz);
+
+    for (int i=0; i<sz; i++) {
+        printf("%d = %c, ASCII = %d\n", i, st[i], st[i]);
+    }
 
     return 0;
 }
