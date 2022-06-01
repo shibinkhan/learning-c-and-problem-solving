e6a04dc80227a6d4e1f3d6b8218180c479b62160
shibinkhan <khanshibin500@gmail.com>
2022-06-01 16:42:26 +0600

    updating

--- 12 - array/input_output.c
+++ 12 - array/input_output.c
@@ -4,11 +4,11 @@ int main() {
     printf("Enter your Name: ");
 
     char name[100];
-    scanf("%s", name);
-    printf("Hello, %s\n...What the fuck do you want?", name);
+    //scanf("%s", name);
+    //printf("Hello, %s\n...What the fuck do you want?", name);
 
     gets(name);
-    printf("Hello, %s\n...What the fuck do you want?", name);
+    puts(name);
 
     // int sz = sizeof(st) / sizeof(st[0]);
 
