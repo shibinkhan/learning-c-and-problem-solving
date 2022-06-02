36872c089529849df4b553049a694e819683a39b
shibinkhan <khanshibin500@gmail.com>
2022-06-02 23:49:37 +0600

    updating

--- 13 - string/str_concat.c
+++ 13 - string/str_concat.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
+    char a[] = "Hello";
+    int aLenght = strlen(a);
+
+    char b[] = "World";
+    int bLenght = strlen(b);
+
+    int cLenght = aLenght + bLenght + 1;
+    char c[cLenght];
+
+
 
 
     return 0;
