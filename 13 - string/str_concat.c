8fc2276b8a7e9ac2c7a9fb831a14ad21f21b5934
shibinkhan <khanshibin500@gmail.com>
2022-06-03 00:12:52 +0600

    updating

--- 13 - string/str_concat.c
+++ 13 - string/str_concat.c
@@ -8,10 +8,18 @@ int main() {
     char b[] = "World";
     int bLenght = strlen(b);
 
-    int cLenght = aLenght + bLenght + 1;
-    char c[cLenght];
+    int cLenght = aLenght + bLenght;
+    char c[cLenght+1];
 
+    for (int i=0; i<aLenght; i++) {
+        c[i] = a[i];
+    }
+    for (int i=0; i<bLenght; i++) {
+        c[aLenght+i] = b[i];
+    }
+    c[cLenght] = '\0';
 
+    printf("%s", c);
 
 
     return 0;
