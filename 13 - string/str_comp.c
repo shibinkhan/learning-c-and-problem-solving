c0036267bb66c36e2e894916555999362d9903ce
shibinkhan <khanshibin500@gmail.com>
2022-06-03 22:30:10 +0600

    updating

--- 13 - string/str_comp.c
+++ 13 - string/str_comp.c
@@ -1,10 +1,48 @@
+/*
+// slow way
+
 #include<stdio.h>
 #include<string.h>
 
 int main() {
-    char a[100];
-    scanf("%s", a);
-    printf("%s", a);
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+
+    int indx = 0;
+    while (a[indx] == b[indx] && a[indx] != '\0' && b[indx] != '\0') {
+        indx ++;
+    }
+    if (a[indx] == '\0' && b[indx] == '\0') {
+        printf("Equal");
+    }
+    else if (a[indx] == '\0' && b[indx] != '\0') {
+        printf("A is Smaller");
+    }
+    else if (a[indx] != '\0' && b[indx] == '\0') {
+        printf("B is Smaller");
+    }
+    else if (a[indx] < b[indx]) {
+        printf("A is Smaller");
+    }
+    else {
+        printf("B is Smaller");
+    }
+
+    return 0;
+}
+*/
+
+// fast way
+
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+
+    int indx = 0;
+
 
     return 0;
 }
