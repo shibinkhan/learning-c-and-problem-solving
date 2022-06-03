d9cf7395c9ad6cdef402612f4bfd6c1024e700ab
shibinkhan <khanshibin500@gmail.com>
2022-06-04 00:58:05 +0600

    updating

--- 14 - lab chass 4/problem_8.c
+++ 14 - lab chass 4/problem_8.c
@@ -3,7 +3,22 @@
 #include<stdio.h>
 
 int main() {
+    int sz = 100;
+    char str[sz];
+    scanf("%s", str);
 
+    int length = strlen(str);
+    // printf("%d", length);
+
+    char result[sz];
+
+    for (int i=0; i<length; i++) {
+        result[i] = str[length-i];
+    }
+    result[length+1] = '\0';
+
+    printf("%s", result);
+    printf("%d", strlen(result));
 
     return 0;
 }
