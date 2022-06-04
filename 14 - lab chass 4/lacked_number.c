a988d9228fb1154aa5c68b8cd4f19268732a0527
shibinkhan <khanshibin500@gmail.com>
2022-06-04 18:06:32 +0600

    updating

---
+++ 14 - lab chass 4/lacked_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    char string[10];
+    scanf("%s", string);
+    printf("%s", string);
+
+
+
+    return 0;
+}
--- 14 - lab chass 4/reverse_array_numbers.c
+++ 14 - lab chass 4/reverse_array_numbers.c
@@ -16,6 +16,13 @@ int main() {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+
+        /*
+        for (int i=0; i<length; i++) {
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+        */
     }
 
     for (int i=0; i<length; i++) {
