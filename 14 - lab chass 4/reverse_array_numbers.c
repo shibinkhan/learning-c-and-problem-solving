ba0b0b03745b5d31aca2ef84860b115d7c6d0d82
shibinkhan <khanshibin500@gmail.com>
2022-06-04 15:17:09 +0600

    updating

---
+++ 14 - lab chass 4/reverse_array_numbers.c
@@ -0,0 +1,23 @@
+// Write a C program to find reverse of a given array of numbers.
+
+#include<stdio.h>
+
+int main() {
+    int sz = 100;
+    char input[sz];
+    scanf("%s", input);
+
+    int lengthInput = strlen(input);
+    input[lengthInput] = '\0';
+
+    char result[sz];
+
+    for (int i=0; i<lengthInput; i++) {
+        result[i] = input[lengthInput-(i+1)];
+    }
+    result[lengthInput] = '\0';
+
+    printf("%s", result);
+
+    return 0;
+}
