c03c2b7523765e7deda2700c1f9334e1b2594b36
shibinkhan <khanshibin500@gmail.com>
2022-06-04 18:38:30 +0600

    updating

--- 14 - lab chass 4/lacked_number.c
+++ 14 - lab chass 4/lacked_number.c
@@ -3,8 +3,17 @@
 int main() {
     char string[10];
     scanf("%s", string);
-    printf("%s", string);
 
+    int freq[10] = {0};
+    for (int i=0; i<9; i++) {
+        int digit = string[i];
+        freq[digit-48] ++;
+    }
+    for (int i=0; i<=9; i++) {
+        if (freq[i] == 0) {
+            printf("%d", i);
+        }
+    }
 
 
     return 0;
