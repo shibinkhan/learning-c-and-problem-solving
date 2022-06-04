580df243fab2970c5e8644755dff4fd8e2294e09
shibinkhan <khanshibin500@gmail.com>
2022-06-04 19:39:13 +0600

    updating

--- 14 - lab chass 4/digit_machine.c
+++ 14 - lab chass 4/digit_machine.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
 
 int main() {
+    int numbers[11];
+    for (int i=0; i<=9; i++) {
+        scanf("%d", &numbers[i]);
+    }
 
+    int current = 0;
+    for (int i=0; i<3; i++) {
+        current = numbers[current];
+    }
+    printf("%d", current);
 
     return 0;
 }
+
+/**
+
+0 1 2 3 4 5 6 7 8 9
+9 0 1 2 3 4 5 6 7 8
+
+**/
