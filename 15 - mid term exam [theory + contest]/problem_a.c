4b922b4d6a62a34097633e29d83658d7c5175899
shibinkhan <khanshibin500@gmail.com>
2022-06-09 00:09:46 +0600

    updating

--- 15 - mid term exam [theory + contest]/problem_a.c
+++ 15 - mid term exam [theory + contest]/problem_a.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
+    char un[101];
+    scanf("%s", un);
+    int length = strlen(un);
 
+    int nums[length+1];
+    for (int i=0; i<length; i++) {
+        nums[i] = un[i];
+    }
+    int ans = 0;
+    for (int i=0; i<length; i++) {
+        int current = nums[i];
+
+        for (int j=0; j<length; j++) {
+            int compare = nums[j];
+
+            if (j==i) continue;
+            if (current == compare) {
+                ans ++;
+            }
+        }
+    }
+
+    int distinct = length - ans;
+
+    if (distinct%2 == 0) {
+        printf("CHAT WITH HER!");
+    }
+    else {
+        printf("IGNORE HIM!");
+    }
 
     return 0;
 }
