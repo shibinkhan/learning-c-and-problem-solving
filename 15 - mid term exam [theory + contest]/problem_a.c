e345cb5253ddf31899ae2b15171ada693ee01cc3
shibinkhan <khanshibin500@gmail.com>
2022-06-09 00:14:53 +0600

    updating

--- 15 - mid term exam [theory + contest]/problem_a.c
+++ 15 - mid term exam [theory + contest]/problem_a.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%s", un);
     int length = strlen(un);
 
-    int nums[length+1];
+    int nums[length];
     for (int i=0; i<length; i++) {
         nums[i] = un[i];
     }
@@ -35,3 +35,36 @@ int main() {
 
     return 0;
 }
+
+/*
+// simplified
+
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char un[101];
+    scanf("%s", un);
+    int length = strlen(un);
+
+    int ans = 0;
+    for (int i=0; i<length; i++) {
+        for (int j=0; j<length; j++) {
+            if (j==i) continue;
+            if (un[i] == un[j]) {
+                ans ++;
+            }
+        }
+    }
+
+    int distinct = length - ans;
+    if (distinct%2 == 0) {
+        printf("CHAT WITH HER!");
+    }
+    else {
+        printf("IGNORE HIM!");
+    }
+
+    return 0;
+}
+*/
