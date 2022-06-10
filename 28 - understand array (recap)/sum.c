fcc28e2fbd67af99b4cd6896478f6baf466fbccf
shibinkhan <khanshibin500@gmail.com>
2022-06-10 22:03:59 +0600

    updating

--- 28 - understand array (recap)/sum.c
+++ 28 - understand array (recap)/sum.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
 int main() {
-    int n = 5;
-    int array[n];
+    int array[5] = {1, 2, 3, 4, 5};
 
+    int sum = 0;
+    for (int i=0; i<5; i++) {
+        // printf("%d ", array[i]);
+        sum += array[i];
+    }
+    printf("%d", sum);
 
     return 0;
 }
