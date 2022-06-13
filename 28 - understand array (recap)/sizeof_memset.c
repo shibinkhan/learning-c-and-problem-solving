f9259b4c8c0992d22916b41b33b86e56868e7eb0
shibinkhan <khanshibin500@gmail.com>
2022-06-13 23:04:29 +0600

    updating

--- 28 - understand array (recap)/sizeof_memset.c
+++ 28 - understand array (recap)/sizeof_memset.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
 int main() {
+    int n;
+    scanf("%d", &n);
 
+    int array[n];
+    // int sz = sizeof(array) / sizeof(array[0]);
+    // printf("%d", sz);
+
+    // memset
+    memset(array, -1, sizeof(array));
+    for (int i=0; i<n; i++) {
+        printf("i = %d, value = %d\n", i, array[i]);
+    }
 
     return 0;
 }
