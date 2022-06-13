d31d02f9f19f0c43a77b318f6e473b364797f380
shibinkhan <khanshibin500@gmail.com>
2022-06-13 23:14:55 +0600

    updating

--- 28 - understand array (recap)/array_as_counter.c
+++ 28 - understand array (recap)/array_as_counter.c
@@ -12,6 +12,14 @@ int main() {
     int count1 = 0;
     int count2 = 0;
 
+    for (int i=0; i<n; i++) {
+        if (array[i] == 1) {
+            count1 ++;
+        }
+        if (array[i] == 2) {
+            count2 ++;
+        }
+    }
 
 
     return 0;
