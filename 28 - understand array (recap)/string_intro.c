9fb874446e13a94e811f5498711e3c7787694e85
shibinkhan <khanshibin500@gmail.com>
2022-06-21 21:24:54 +0600

    updating

--- 28 - understand array (recap)/string_intro.c
+++ 28 - understand array (recap)/string_intro.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
 int main() {
+    int str[5] = {'a', 'b', 'c', 'd', 'e'};
+    int str2[6] = "abcde";
+    int str3[5];
 
+    for (int i=0; i<5; i++) {
+        scanf("%c", &str3[i]);
+    }
+    for (int i=0; i<5; i++) {
+        printf("%c", str3[i]);
+    }
 
     return 0;
 }
