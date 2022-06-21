4903e6deab3649426c6b5b420d6e10eb04da8607
shibinkhan <khanshibin500@gmail.com>
2022-06-21 23:56:34 +0600

    updating

--- 28 - understand array (recap)/counter.c
+++ 28 - understand array (recap)/counter.c
@@ -7,6 +7,10 @@ int main() {
     int length = strlen(str);
     int counter[27] = {0};
 
+    for (int i=0; i<length; i++) {
+        char val = str[i];
+        counter[val-97] ++;
+    }
 
     return 0;
 }
